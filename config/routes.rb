0bbb9840517340ed2a54fa524a8eee7b53af3520
Sebastián Fernández <sebacontacto@gmail.com>
2023-07-20 11:56:36 -0400

    feat: navbar en todas las vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'contact/content'
+  get "/contact", to: "contact#content"
   get 'projects/content'
+  get "/projects", to: "projects#content"
   get 'home/content'
+  get "/home", to: "home#content"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  
-
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#content"
 end
